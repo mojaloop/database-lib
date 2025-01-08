d5b3b3eba4f9b6f2439f984150f4b49cde80feb8
Juan Correa <gibaros@users.noreply.github.com>
2025-01-08 10:25:34 -0500

    chore: rm ncurc.js, update deps

--- .ncurc.js
+++
@@ -1,8 +0,0 @@
-// .ncurc.js
-module.exports = {
-  // ... other ncu configuration ...
-
-  reject: [
-    'npm-check-updates'  // Per .nvmrc Node.js version cannot further update this lib
-  ],
-};
